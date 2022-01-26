ef8e4d2c9b4ecdee96bc2324c1106d8e8f60bfc2
Michael Klein <Michaelklein1997@gmail.com>
2022-01-26 16:02:10 -0600

    downloaded inquirer, created prompts, and began function format

--- main/index.js
+++ main/index.js
@@ -9,4 +9,80 @@ const htmlTemplate = ('./src/html-template')
 
 // Import needed packages
 const fs = require('fs');
-const inquirer = require('requirer');
+const inquirer = require('inquirer');
+
+let role = 'Manager'
+const questionsAll = [
+    {
+        type: 'input',
+        message: `Please enter the ${role}'s name.`,
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: `Please enter the ${role}'s employee ID.`,
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: `Please enter the ${role}'s email address.`,
+        name: 'email',
+    },
+]
+
+const questionMenu = [
+    {
+        type: 'list',
+        message: 'Add another member to your team?',
+        choices: ['Engineer', 'Intern', 'No']
+    }
+]
+
+const questionsManager = [
+    {
+        type: 'input',
+        message: "Please enter the Manager's room number",
+        name: 'roomNumber',
+    },
+]
+
+const questionsEngineer = [
+    {
+        type: 'input',
+        message: "Please enter the Engineers's GitHub username",
+        name: 'roomNumber',
+    },
+]
+
+const questionsIntern = [
+    {
+        type: 'input',
+        message: "Please enter the Intern's school",
+        name: 'roomNumber',
+    },
+]
+
+function addMembers() {
+    inquirer.prompt(questionsAll).then(pushMember)
+
+}
+
+function pushMember(answers) {
+    answers.role === undefined ? answers.role = 'Manager' : answers.role = role
+    switch (answers.role) {
+        case 'Manager':
+            inquirer.prompt(questionsManager).then(showAnswers);
+            break;
+        case 'Engineer':
+            inquirer.prompt(questionsEngineer).then(showAnswers);
+            break;
+        case 'Intern':
+            inquirer.prompt(questionsIntern).then(showAnswers)
+            break
+    }
+}
+
+function showAnswers(answers) {
+    console.log(answers)
+}
+addMembers()
