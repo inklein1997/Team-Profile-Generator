ab956a4627fbf7d84abbbffb30802e1bdbb1ab57
Michael Klein <Michaelklein1997@gmail.com>
2022-01-26 15:05:49 -0600

    added needed imports to index.js

---
+++ main/index.js
@@ -0,0 +1,12 @@
+// Import Class constructors
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+
+// Import needed files
+const htmlTemplate = ('./src/html-template')
+
+// Import needed packages
+const fs = require('fs');
+const inquirer = require('requirer');
