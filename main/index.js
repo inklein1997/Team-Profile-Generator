1d246422d762e0a80751324e4d19ca4a4a48eaa2
Michael Klein <Michaelklein1997@gmail.com>
2022-01-26 17:35:35 -0600

    added template files.  Also Inquirer now asks specific questions based on role

--- main/index.js
+++ main/index.js
@@ -5,13 +5,16 @@ const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
 // Import needed files
-const htmlTemplate = ('./src/html-template')
+const generateHTML = ('./src/generateHTML')
+const generateCard = ('./src/generateCard')
 
 // Import needed packages
 const fs = require('fs');
 const inquirer = require('inquirer');
 
 let role = 'Manager'
+const teamMembers = []
+
 const questionsAll = [
     {
         type: 'input',
@@ -28,61 +31,61 @@ const questionsAll = [
         message: `Please enter the ${role}'s email address.`,
         name: 'email',
     },
+    {
+        type: 'input',
+        message: "Please enter the Manager's room number",
+        name: 'roomNumber',
+        when: answers => role == 'Manager'
+    },
+    {
+        type: 'input',
+        message: "Please enter the Engineers's GitHub username",
+        name: 'github',
+        when: answers => role == 'Engineer',
+    },
+    {
+        type: 'input',
+        message: "Please enter the Intern's school",
+        name: 'school',
+        when: answers => role == 'Intern'
+    },
 ]
 
 const questionMenu = [
     {
         type: 'list',
         message: 'Add another member to your team?',
-        choices: ['Engineer', 'Intern', 'No']
+        choices: ['Engineer', 'Intern', 'No'],
+        name: 'role'
     }
 ]
 
-const questionsManager = [
-    {
-        type: 'input',
-        message: "Please enter the Manager's room number",
-        name: 'roomNumber',
-    },
-]
-
-const questionsEngineer = [
-    {
-        type: 'input',
-        message: "Please enter the Engineers's GitHub username",
-        name: 'roomNumber',
-    },
-]
-
-const questionsIntern = [
-    {
-        type: 'input',
-        message: "Please enter the Intern's school",
-        name: 'roomNumber',
-    },
-]
-
-function addMembers() {
+function addMembers(role) {
+    (role === undefined) ? role = 'Manager' : role
     inquirer.prompt(questionsAll).then(pushMember)
-
 }
 
 function pushMember(answers) {
     answers.role === undefined ? answers.role = 'Manager' : answers.role = role
-    switch (answers.role) {
-        case 'Manager':
-            inquirer.prompt(questionsManager).then(showAnswers);
-            break;
-        case 'Engineer':
-            inquirer.prompt(questionsEngineer).then(showAnswers);
-            break;
-        case 'Intern':
-            inquirer.prompt(questionsIntern).then(showAnswers)
-            break
-    }
+    teamMembers.push(answers)
+    inquirer.prompt(questionMenu).then((answer) => {
+        role = menu(answer)
+        menu(answer) !== 'No' ? addMembers(menu(answer)) : showAnswers()
+    })
 }
 
-function showAnswers(answers) {
-    console.log(answers)
+const menu = (answer) => {
+    switch (answer.role) {
+        case 'Intern':
+            return 'Intern'
+        case 'Engineer':
+            return 'Engineer'
+        case 'No':
+            return 'No'
+    }
+    console.log(`new role is ${role}`)
+}
+function showAnswers() {
+    console.log(teamMembers)
 }
 addMembers()
---
+++ main/src/generateCard.js
@@ -0,0 +1,34 @@
+
+
+const cardTemplate = () => {
+    return `<div class="card shadow-lg" style="width: 18rem;">
+    <div class="card-header bg-primary text-white">
+        <h2>Michael Klein</h2>
+        <h2>Role</h2>
+    </div>
+    <div class="card-body">
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: 1</li>
+          <li class="list-group-item">Email: michaelklein1997@gmail.com</li>
+          <li class="list-group-item">Office Number</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="card shadow-lg" style="width: 18rem;">
+    <div class="card-header bg-primary text-white">
+        <h2>Michael Klein</h2>
+        <h2>Role</h2>
+    </div>
+    <div class="card-body">
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: 1</li>
+          <li class="list-group-item">Email: michaelklein1997@gmail.com</li>
+          <li class="list-group-item">Office Number</li>
+        </ul>
+      </div>
+    </div>
+  </div>`
+}
